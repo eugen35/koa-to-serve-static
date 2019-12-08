008e8b28fb5519c21cb8425ec2caa2a7d9da9960
_meugen_ <eugen35@mail.ru>
2019-12-08 12:32:01 +0500

    first commit

--- babelasync.js
+++ babelasync.js
@@ -8,20 +8,18 @@ const co = require( 'co' )
 
 const app = new Koa()
 const io = new IO()
-const chat = new IO( 'chat' )
 
 io.attach( app )
-chat.attach( app )
 
 /**
  * Koa Middlewares
  */
-app.use( co.wrap( function *( ctx, next ) {
+app.use( async ( ctx, next ) => {
   const start = new Date
-  yield next()
+  await next()
   const ms = new Date - start
   console.log( `${ ctx.method } ${ ctx.url } - ${ ms }ms` )
-}))
+})
 
 
 /**
@@ -33,35 +31,32 @@ app.use( ctx => {
 })
 
 /**
- * Socket middlewares
+ * io middlewares
  */
-io.use( co.wrap( function *( ctx, next ) {
-  console.log( 'Socket middleware' )
+io.use( async ( ctx, next ) => {
+  console.log( 'io middleware' )
   const start = new Date
-  yield next()
+  await next()
   const ms = new Date - start
   console.log( `WS ${ ms }ms` )
-}))
-io.use( co.wrap( function *( ctx, next ) {
+})
+io.use( async ( ctx, next ) => {
   ctx.teststring = 'test'
-  yield next()
-}))
+  await next()
+})
 
 /**
- * Socket handlers
+ * io handlers
  */
 io.on( 'connection', ctx => {
   console.log( 'Join event', ctx.socket.id )
   io.broadcast( 'connections', {
     numConnections: io.connections.size
   })
-  // app.io.broadcast( 'connections', {
-  //   numConnections: socket.connections.size
-  // })
 })
 
 io.on( 'disconnect', ctx => {
-  console.log( 'leave event', ctx.socket.id )
+  console.log( 'leave event', ctx.io.id )
   io.broadcast( 'connections', {
     numConnections: io.connections.size
   })
@@ -74,32 +69,10 @@ io.on( 'data', ( ctx, data ) => {
     message: 'response from server'
   })
 })
-io.on( 'ack', ( ctx, data ) => {
-  console.log( 'data event with acknowledgement', data )
-  ctx.acknowledge( 'received' )
-})
 io.on( 'numConnections', packet => {
   console.log( `Number of connections: ${ io.connections.size }` )
 })
 
-/**
- * Chat handlers
- */
-chat.on( 'connection', ctx => {
-  console.log( 'Joining chat namespace', ctx.socket.id )
-})
-chat.on( 'message', ctx => {
-  console.log( 'chat message received', ctx.data )
-
-  // Broadcasts to everybody, including this connection
-  app.chat.broadcast( 'message', 'yo connections, lets chat' )
-
-  // Broadcasts to all other connections
-  ctx.socket.broadcast( 'message', 'ok connections:chat:broadcast' )
-
-  // Emits to just this socket
-  ctx.socket.emit( 'message', 'ok connections:chat:emit' )
-})
 
 const PORT = 3000
 app.listen( 3000, () => {
