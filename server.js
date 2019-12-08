68bc70d52902ffcd15e1cb777591ff6bc6cc40ca
_meugen_ <eugen35@mail.ru>
2019-12-08 11:51:15 +0500

    Оставил только koa-ejs

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
-const send = require('koa-send');
+'use strict';
+
 const Koa = require('koa');
-const app = new Koa();
+const render = require('koa-ejs');
+const path = require('path');
 
-// $ GET /package.json
-// $ GET /
+const server = new Koa();
 
-app.use(async (ctx) => {
-  if ('/' == ctx.path) return ctx.body = 'Try GET /package.json';
-  await send(ctx, ctx.path);
-})
+render(server, {
+  root: path.join(__dirname, 'view'),
+  layout: 'template',
+  viewExt: 'html',
+  cache: false,
+  debug: false,
+});
 
-app.listen(3000);
-console.log('listening on port 3000');
+server.use(function (ctx, next) {
+  ctx.state = ctx.state || {};
+  ctx.state.now = new Date();
+  ctx.state.ip = ctx.ip;
+  ctx.state.version = '2.0.0';
+  return next();
+});
+
+server.use(async function (ctx) {
+  const users = [{ name: 'Dead Horse' }, { name: 'Jack' }, { name: 'Tom' }];
+  await ctx.render('content', {
+    users
+  });
+});
+
+if (process.env.NODE_ENV === 'test') {
+  module.exports = server.callback();
+} else {
+  server.listen(7001);
+  console.log('open http://localhost:7001');
+}
+
+server.on('error', function (err) {
+  console.log(err.stack);
+});
